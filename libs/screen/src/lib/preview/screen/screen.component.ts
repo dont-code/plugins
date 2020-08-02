521a7315f5b6b85b08e7e322c03d9c1cbd73144f
Gérard Collin <gerard.collin@allianz.com>
2020-08-02 16:40:04 +0200

    Plugin listens to events

--- libs/screen/src/lib/preview/screen/screen.component.ts
+++ libs/screen/src/lib/preview/screen/screen.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { CommandProviderInterface, PreviewHandler } from "@dontcode/core";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   selector: 'plugins-screen',
   templateUrl: './screen.component.html',
   styleUrls: ['./screen.component.css']
 })
-export class ScreenComponent implements OnInit {
+export class ScreenComponent implements OnInit, OnDestroy, PreviewHandler {
+
+  unsubscriber = new Subject();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  initCommandFlow(provider: CommandProviderInterface, position: string, schemaPosition: string) {
+//    console.log("InitCommandFlow");
+    provider.receiveCommands(position).pipe(
+      takeUntil(this.unsubscriber)
+    ).subscribe(change => {
+      console.log("Getting updates from ", position, " with value ", change.value);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.forceUnsubscribe ();
+  }
+
+  forceUnsubscribe (): void {
+    // unsubscribe to all observables
+//    console.log("Unsubscribe");
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+    this.unsubscriber = new Subject();
   }
 
 }
