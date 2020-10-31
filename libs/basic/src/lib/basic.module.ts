24667f1d7006bb86f8754a4449d11e50d215bbe4
Gérard Collin <gerard.collin@allianz.com>
2020-10-31 16:38:43 +0100

    Loading handlers from module

--- libs/basic/src/lib/basic.module.ts
+++ libs/basic/src/lib/basic.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DontCode } from "@dontcode/core";
+import { DontCode, PluginModuleInterface } from "@dontcode/core";
 import dtcde = DontCode.dtcde;
 import { BasicPlugin } from "./declaration/basic-plugin";
 import { BasicEntityComponent } from './preview/entity/basic-entity.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BasicEntityComponent]
+  declarations: [BasicEntityComponent],
+  id:'dontcode-plugin/basic'
 })
-export class BasicModule {
+export class BasicModule implements PluginModuleInterface {
   constructor() {
     console.log("Basic Plugin registering");
     dtcde.registerPlugin(new BasicPlugin ());
   }
+
+  // We declare the components
+  exposedPreviewHandlers(): Map<string, any> {
+        return new Map<string, any> ([['BasicEntityComponent', BasicEntityComponent]]);
+    }
 
 }
 
--- libs/screen/src/lib/screen.module.ts
+++ libs/screen/src/lib/screen.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DontCode } from "@dontcode/core";
+import { DontCode, PluginModuleInterface } from "@dontcode/core";
 import dtcde = DontCode.dtcde;
 import { ScreenPlugin } from "./declaration/screen-plugin";
 import { ScreenComponent } from './preview/screen/screen.component';
+import { BasicEntityComponent } from "@dontcode/plugin-basic";
 
 @NgModule({
   imports: [CommonModule],
   declarations: [ScreenComponent]
 })
-export class ScreenModule {
+export class ScreenModule implements PluginModuleInterface {
   constructor() {
     console.log("Screen Plugin registering");
     dtcde.registerPlugin(new ScreenPlugin ());
+  }
+  // We declare the components
+  exposedPreviewHandlers(): Map<string, any> {
+    return new Map<string, any> ([['ScreenComponent', ScreenComponent]]);
   }
 }
 
