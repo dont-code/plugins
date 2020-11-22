40a6bc5cba2d9275339d3c8c4023b610b91a5fc0
Gérard Collin <gerard.collin@allianz.com>
2020-11-22 12:24:39 +0100

    auto update before ci

--- update-core.bat
+++ update-core.bat
@@ -1,5 +1,8 @@
-cd ../core/node
+cd ..\core\node
 call nx run core:build --prod
 call npm pack dist/libs/core
-cd ../../plugins
-call npm install ../core/node/dontcode-core-0.1.18.tgz
+cd ..\..\plugins
+move ..\core\node\dontcode-core-*.tgz .
+del dontcode-core-dev.tgz
+ren dontcode-core-*.tgz dontcode-core-dev.tgz
+call npm install dontcode-core-dev.tgz
