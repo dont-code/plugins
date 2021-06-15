c33f45173dcb565d66addf3822cbd3c440589630
Gérard Collin <gerard.collin@appliweb.net>
2021-06-15 10:47:52 +0200

    Add support for Store Mgt plugins

--- libs/basic/src/lib/declaration/basic-plugin.ts
+++ libs/basic/src/lib/declaration/basic-plugin.ts
@@ -18,7 +18,23 @@ export class BasicPlugin implements Plugin
           },
           "replace": false
         }]
-      }],
+      },
+        {
+          "id": "dont-code-storage",
+          "description": "Stores application data in Dont-code project storage",
+          "changes": [{
+            "location": {
+              "parent": "#/definitions/sharing",
+              "id":"with"
+            },
+            "replace": false,
+            "add": {
+              "enum": [
+                "Dont-code users"
+              ]
+            }
+          }]
+        }],
       "preview-handlers": [
         {
           location: {
@@ -39,8 +55,18 @@ export class BasicPlugin implements Plugin
             name:'BasicFieldsComponent',
             source:'basic'
           }
+        },
+        {
+          location: {
+            parent: DontCodeModel.APP_SHARING,
+            id: 'with',
+            values: ['Dont-code users']
+          },
+          class: {
+            name:'BasicFieldsComponent',
+            source:'basic'
+          }
         }
-
       ]
     }
   }
