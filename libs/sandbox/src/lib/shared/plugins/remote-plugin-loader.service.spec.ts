6c5d49ce845d0330fc4f01968626543fc6824393
Gérard Collin <gerard.collin@appliweb.net>
2022-06-18 10:56:19 +0200

    Support loading repository definitions

--- libs/sandbox/src/lib/shared/plugins/remote-plugin-loader.service.spec.ts
+++ libs/sandbox/src/lib/shared/plugins/remote-plugin-loader.service.spec.ts
@@ -1,17 +1,97 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { RemotePluginLoaderService } from './remote-plugin-loader.service';
+import {RemotePluginLoaderService, RemotePluginModuleOptions} from './remote-plugin-loader.service';
+import {dtcde, PluginModuleInterface} from "@dontcode/core";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 describe('RemotePluginLoaderService', () => {
   let service: RemotePluginLoaderService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule ], providers: [
+        {provide: RemotePluginLoaderService, useClass: TestRemotePluginLoaderService}
+      ]});
     service = TestBed.inject(RemotePluginLoaderService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should load default plugin config', (done) => {
+    service.loadPluginsFromRepository("assets/repositories/test.json","assets/repositories/default.json").then(value => {
+      expect (value.plugins).toHaveLength(2);
+      const configs=service.listAllRepositoryConfigUpdates();
+      expect(configs).toHaveLength(1);
+      dtcde.getModelManager().applyPluginConfigUpdates(configs);
+      const added = dtcde.getModelManager().findAtPosition("creation/sources/a");
+      expect(added).toEqual({
+        name:"Test"
+      });
+      done();
+    }).catch(error => {
+      done(error);
+    })
+    httpTestingController.match("assets/repositories/test.json").forEach(value => {
+      value.flush(TEST_JSON);
+    })
+
+    httpTestingController.verify();
+  });
+
 });
+
+const TEST_JSON={
+  "$schema": "https://dont-code.net/schemas/v1/repository-schema.json",
+  "name": "Default Repository",
+  "description": "Repository provided by Dont-code offering all defaults plugins",
+  "plugins": [
+  {
+    "id": "Basic",
+    "display-name": "Test Basic plugin with no config",
+    "version": "1.0.0",
+    "config": {}
+  },
+  {
+    "id": "BasicConfig",
+    "display-name": "Basic plugin again with config",
+    "version": "1.0.0",
+    "info": {
+
+    },
+    "config": {
+      "definition-updates": [{
+        "location": {
+          "parent": "creation/sources",
+          "id": "*"
+        },
+        "update": {
+          "name": "Test"
+        }
+      }
+    ]
+
+    }
+  }
+]
+}
+
+/**
+ * Overrides the dynamic loading of modules as obviously this will not work in unit test
+ */
+class TestRemotePluginLoaderService extends RemotePluginLoaderService {
+
+  override async loadModule(moduleDef: RemotePluginModuleOptions): Promise<PluginModuleInterface> {
+    return Promise.resolve(new TestPluginModuleInterface ());
+  }
+}
+
+class TestPluginModuleInterface implements PluginModuleInterface{
+  exposedPreviewHandlers(): Map<string, any> {
+    return new Map<string, any>();
+  }
+
+}
--- libs/sandbox/src/lib/shared/plugins/remote-plugin-loader.service.ts
+++ libs/sandbox/src/lib/shared/plugins/remote-plugin-loader.service.ts
@@ -1,6 +1,8 @@
 import {createNgModuleRef, Injectable, Injector} from '@angular/core';
 import {loadRemoteModule, LoadRemoteModuleOptions,} from '@angular-architects/module-federation-runtime';
-import {PluginModuleInterface} from '@dontcode/core';
+import {DefinitionUpdateConfig, PluginModuleInterface, RepositoryPluginEntry, RepositorySchema} from '@dontcode/core';
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom} from "rxjs";
 
 export type RemotePluginModuleOptions = LoadRemoteModuleOptions & {
   moduleName: string;
@@ -13,7 +15,96 @@ export type RemotePluginModuleOptions = LoadRemoteModuleOptions & {
   providedIn: 'root',
 })
 export class RemotePluginLoaderService {
-  constructor(protected injector: Injector) {}
+
+  repository:RepositorySchema|null=null;
+
+  constructor( protected injector: Injector, protected httpClient: HttpClient) {}
+
+  /**
+   * Loads the configuration file of the plugin repository, then loads and configure all plugins in it
+   * @param repoUrl
+   */
+  loadPluginsFromRepository (repoUrl:string, defaultRepoUrl:string, ) : Promise<RepositorySchema>{
+/*    this.httpClient.get<RepositorySchema>(repoUrl, {observe:'events', responseType:'json'}).subscribe({
+      next: value => {
+        console.log("Received", value);
+      },
+      error: err => {
+        console.error("Error ", err)
+      },
+      complete: () => {
+        console.log("Complete");
+      }
+    });*/
+
+    return firstValueFrom(
+      this.httpClient.get<RepositorySchema>(repoUrl, {observe:'body', responseType:'json'})
+      ).then((config:RepositorySchema) => {
+        this.repository = config;
+        if (config.plugins != null) {
+          const toLoad=new Array<RemotePluginModuleOptions>();
+          config.plugins.forEach(value => {
+            toLoad.push(this.createRemotePluginConfig (value, defaultRepoUrl));
+          });
+          return this.loadMultipleModules(toLoad);
+        }
+        return Promise.resolve([]);
+    }).then (() => {
+      if( this.repository!=null)
+        return this.repository;
+      else {
+        return Promise.reject("No repository loaded");
+      }
+    })
+      .catch((reason)=> {
+      console.error("Cannot load repository config from "+repoUrl+" due to error ", reason);
+      return Promise.reject(reason);
+    })
+  }
+
+  protected createRemotePluginConfig(value: RepositoryPluginEntry, defaultRepoUrl:string): RemotePluginModuleOptions {
+    let ret:RemotePluginModuleOptions;
+    const upperId = value.id.substring(0,1).toUpperCase()+value.id.substring(1);
+
+    if (value.info != null) {
+      const moduleName=value.info["module-name"]??upperId+'Module';
+      const exposedModule = value.info["exposed-module"]??'./'+upperId;
+      const remoteEntry = value.info["remote-entry"]??defaultRepoUrl+'/remoteEntry.mjs';
+
+      ret = {
+        type: 'module',
+        exposedModule: exposedModule,
+        moduleName: moduleName,
+        remoteEntry: remoteEntry
+      }
+    }else {
+      ret = {
+        type: 'module',
+        exposedModule:'./'+upperId,
+        moduleName: upperId+'Module',
+        remoteEntry:defaultRepoUrl+'/remoteEntry.mjs'
+      }
+    }
+
+    return ret;
+  }
+
+  /**
+   * Returns all the plugin configuration overriden by the repository.
+   */
+  listAllRepositoryConfigUpdates (): Array<DefinitionUpdateConfig> {
+    const ret = new Array<DefinitionUpdateConfig>();
+    this.repository?.plugins?.forEach(value => {
+      if ((value.config != null) && (value.config["definition-updates"] != null)) {
+        value.config["definition-updates"].forEach(update => {
+          ret.push(update);
+          })
+        }
+      }
+    );
+
+    return ret;
+  }
 
   /**
    * Loads a module containing plugins from a remote location using Module Federation
