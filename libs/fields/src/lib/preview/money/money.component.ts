6ba63e547bb3f115f10eef5b093d530b5157ce63
Gérard Collin <gerard.collin@appliweb.net>
2022-09-30 11:18:32 +0200

    Simplified MoneyComponent working

--- libs/fields/src/lib/preview/money/money.component.ts
+++ libs/fields/src/lib/preview/money/money.component.ts
@@ -1,12 +1,12 @@
-import { Component, Injector, TemplateRef, ViewChild } from '@angular/core';
-import { MoneyAmount } from '@dontcode/core';
+import {Component, Injector, TemplateRef, ViewChild} from '@angular/core';
+import {MoneyAmount} from '@dontcode/core';
 import {
   AbstractDynamicLoaderComponent,
   ComponentLoaderService,
   PossibleTemplateList,
   TemplateList,
 } from '@dontcode/plugin-common';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 /**
  * Display or edit a country value
@@ -24,12 +24,12 @@ export class MoneyComponent extends AbstractDynamicLoaderComponent {
   private fullEditView!: TemplateRef<any>;
 
   override value: MoneyAmount = new MoneyAmount();
-  valueAmountDefined = false;
 
   converter = Intl.NumberFormat(navigator.language, { style:'currency', currency:'EUR'});
 
   constructor(injector: Injector, loaderService: ComponentLoaderService) {
     super(loaderService, injector);
+    // We use Dont-code framework to find the component that will manage the currency selection
     this.defineSubField ('currencyCode', 'Currency');
   }
 
@@ -43,39 +43,17 @@ export class MoneyComponent extends AbstractDynamicLoaderComponent {
 
   override setForm(form: FormGroup) {
     super.setForm(form);
-    this.group?.registerControl('amount', new FormControl(this.amount));
+    this.group?.registerControl('amount', new FormControl(this.getAmountSafe()));
   }
 
-  get amount(): number | undefined {
-    if (this.valueAmountDefined) return this.value.amount;
-    else return;
-  }
-
-  set amount(newAmount) {
-    if (newAmount) {
-      this.value.amount = newAmount;
-      this.valueAmountDefined = true;
-    } else {
-      this.valueAmountDefined = false;
-    }
+  getAmountSafe(): number | undefined {
+    return this.value?.amount;
   }
 
   override setValue(val: any) {
     super.setValue(val);
-    if (this.value) {
-      this.valueAmountDefined = true;
-    } else {
-      this.value = new MoneyAmount();
-      this.valueAmountDefined = false;
-    }
     this.updateConverter();
   }
-
-  override getValue(): any {
-    const val = super.getValue() as MoneyAmount;
-    return val;
-  }
-
 
   updateConverter (): void {
     if (this.value?.currencyCode!=null)
@@ -89,21 +67,4 @@ export class MoneyComponent extends AbstractDynamicLoaderComponent {
     return ret;
   }
 
-
-  override transformToSource(type: string, val: any): any {
-    const ret= super.transformToSource(type, val);
-/*    if( !this.valueAmountDefined) {
-      ret.amount = undefined;
-    }*/
-    return ret;
-  }
-
-  override transformFromSource(type: string, val: any): any {
-    if (val?.amount==null) {
-      this.valueAmountDefined=false;
-    } else {
-      this.valueAmountDefined=true;
-    }
-    return super.transformFromSource(type, val);
-  }
 }
